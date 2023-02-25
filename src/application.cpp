9ae0439ddc65324bf9f8ea756f0bd80a3e1d2797
Luis von der Eltz <luisvde@outlook.de>
2023-02-25 00:00:00 +0000

    Move some stuff around

--- src/application.cpp
+++ src/application.cpp
@@ -3,40 +3,8 @@
 #include <application.h>
 #include <config.h>
 
-namespace {
-void setup_serial() {
-  Serial.begin(9600);
-
-#if APC_DEBUG
-  while (!Serial) {
-    // wait for serial port to connect.
-  }
-#endif
-}
-
-time_t get_teensy_time() { return Teensy3Clock.get(); }
-
-void sync_with_RTC() {
-  // set the Time library to use Teensy RTC to keep time
-  setSyncProvider(get_teensy_time);
-
-  if (timeStatus() != timeSet) {
-    Serial.println("Unable to sync with the RTC");
-  } else {
-    Serial.println("RTC has set the system time");
-  }
-}
-
-}  // namespace
-
 apc::application::application()
-    : _running([]() {
-        setup_serial();
-        sync_with_RTC();
-
-        return true;
-      }()),
-      _display(DisplayConfig),
+    : _display(DisplayConfig),
       _controls(ControlsMux1Config),
       _diagScreen(&_usb),
       _mixerScreen(&_audioGraph, &_display, &_browseScreen, &_controls),
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,39 @@
 
 #include <memory>
 
+void setup_serial() {
+  Serial.begin(9600);
+
+#if APC_DEBUG
+  while (!Serial) {
+    // wait for serial port to connect.
+  }
+#endif
+}
+
+time_t get_teensy_time() { return Teensy3Clock.get(); }
+
+void sync_with_RTC() {
+  // set the Time library to use Teensy RTC to keep time
+  setSyncProvider(get_teensy_time);
+
+  if (timeStatus() != timeSet) {
+    Serial.println("Unable to sync with the RTC");
+  } else {
+    Serial.println("RTC has set the system time");
+  }
+}
+
 std::unique_ptr<apc::application> app;
 
 void setup() {
+  setup_serial();
+  sync_with_RTC();
+
+#if APC_DEBUG
   apc::init_debugger();
+#endif
+
   app = std::make_unique<apc::application>();
 }
 
