209f87962d810e067906ff5fb8ec534c5720edea
Luis von der Eltz <luisvde@outlook.de>
2023-04-27 00:00:00 +0000

    Tweak filter coeffs

--- src/audio/equalizer.cpp
+++ src/audio/equalizer.cpp
@@ -1,13 +1,14 @@
 #include <audio/equalizer.h>
+#include <logger.h>
 
 apc::audio::equalizer::equalizer(std::array<filter_bank, 2> filterBanks)
     : _filterBanks(filterBanks) {
-  _filterBanks[0].low->setBandpass(0, 80, 1.0f);
-  _filterBanks[0].low->setBandpass(0, 80, 1.0f);
-  _filterBanks[1].mid->setBandpass(0, 1200, 1.0f);
-  _filterBanks[1].mid->setBandpass(0, 1200, 1.0f);
-  _filterBanks[2].high->setBandpass(0, 10000, 1.0f);
-  _filterBanks[2].high->setBandpass(0, 10000, 1.0f);
+  _filterBanks[0].low->setBandpass(0, 80, 0.4f);
+  _filterBanks[0].low->setBandpass(0, 80, 0.4f);
+  _filterBanks[1].mid->setBandpass(0, 1200, 0.4f);
+  _filterBanks[1].mid->setBandpass(0, 1200, 0.4f);
+  _filterBanks[2].high->setBandpass(0, 10000, 0.4f);
+  _filterBanks[2].high->setBandpass(0, 10000, 0.4f);
 }
 
 void apc::audio::equalizer::setBass(float v) {
